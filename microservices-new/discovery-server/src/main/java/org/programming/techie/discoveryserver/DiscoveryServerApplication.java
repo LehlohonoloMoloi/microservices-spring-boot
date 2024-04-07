f4d4695f67c2949d281ae0d57df2b9d40d4f40af
Lehlohonolo Moloi <lehlohonolo@bbd.co.za>
2024-04-07 03:20:04 +0200

    Added an api gateway.

--- microservices-new/discovery-server/src/main/java/org/programming/techie/discoveryserver/DiscoveryServerApplication.java
+++ microservices-new/discovery-server/src/main/java/org/programming/techie/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package org.programming.discoveryserver;
+package org.programming.techie.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
